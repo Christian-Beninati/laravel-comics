41ee1297ff27b938e570854e9964962f6bd63664
Christian-Beninati <christianbeninati.info@gmail.com>
2023-08-03 15:46:58 +0100

    changes in Home.blade.php

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,21 +1,3 @@
-<!DOCTYPE html>
-<html lang="en">
+@extends('layouts.main')
 
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>{{ env('APP_NAME') }} | Home</title>
-    <link rel="icon" href="{{ Vite::asset('resources/img/favicon.ico') }}">
-
-    {{-- Import asset --}}
-    @vite('resources/js/app.js')
-</head>
-
-<body>
-    <h1>
-        Hello word
-    </h1>
-</body>
-
-</html>
+@section('title', 'Home')
