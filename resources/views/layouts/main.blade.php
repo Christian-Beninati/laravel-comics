0961a2a8b0b119250ff2e7070c81cb5d5e3c7d84
Christian-Beninati <christianbeninati.info@gmail.com>
2023-08-03 15:45:42 +0100

    main.blade.php

---
+++ resources/views/layouts/main.blade.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    {{-- Title --}}
+    <title>{{ env('APP_NAME') }} | @yield('title')</title>
+    {{-- Icon --}}
+    <link rel="icon" href="{{ Vite::asset('resources/img/favicon.ico') }}">
+
+    {{-- Import asset --}}
+    @vite('resources/js/app.js')
+</head>
+
+<body>
+
+</body>
+
+</html>
