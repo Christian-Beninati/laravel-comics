c776cdc731aeed7bc141d73038d1ebfe7e570f18
Christian-Beninati <christianbeninati.info@gmail.com>
2023-08-04 12:58:40 +0200

    add header_menu in config

---
+++ config/header_menu.php
@@ -0,0 +1,12 @@
+<?php return [
+    ['text' => 'caracters', 'route' => 'caracters'],
+    ['text' => 'comics', 'route' => 'comics'],
+    ['text' => 'movies', 'route' => 'movies'],
+    ['text' => 'tv', 'route' => 'tv'],
+    ['text' => 'games', 'route' => 'games'],
+    ['text' => 'collectibles', 'route' => 'collectibles'],
+    ['text' => 'videos', 'route' => 'videos'],
+    ['text' => 'fans', 'route' => 'fans'],
+    ['text' => 'news', 'route' => 'news'],
+    ['text' => 'shop', 'route' => 'shop'],
+];
