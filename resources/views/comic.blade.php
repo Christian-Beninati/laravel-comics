f3b827ede272cbc117c9d99fd50132f6af0c5b68
Christian-Beninati <christianbeninati.info@gmail.com>
2023-08-21 18:08:36 +0100

    added the first part (comic)

--- resources/views/comic.blade.php
+++ resources/views/comic.blade.php
@@ -3,3 +3,50 @@
 
 {{-- Title --}}
 @section('title', $comic['series'])
+
+
+@section('main-content')
+    {{-- Comic --}}
+    <section class="comic">
+        {{-- Blue Bar --}}
+        <div class="blue-bar">
+            {{-- Container --}}
+            <div class="container">
+                {{-- Card Comic --}}
+                <div class="card-comic">
+                    <img src="{{ $comic['thumb'] }}" alt='{{ $comic['series'] }}'>
+                </div>
+            </div>
+        </div>
+
+        {{-- Container --}}
+        <div class="container">
+            {{-- Row --}}
+            <div class="row">
+                {{-- Card Left --}}
+                <div class="card-left">
+                    {{-- Title --}}
+                    <h2>{{ $comic['title'] }}</h2>
+                    {{-- Available --}}
+                    <div class="available">
+                        <div class="left-side">
+                            {{-- Price --}}
+                            <div class="price"> U.S. Price: <span>{{ $comic['price'] }}</span></div>
+                            {{-- Status --}}
+                            <div class="status">AVAILABLE</div>
+                        </div>
+                        {{-- Check --}}
+                        <div class="right-side">Check Availability</div>
+                    </div>
+                    {{-- Desciption --}}
+                    <p class="description">{{ $comic['description'] }}</p>
+                </div>
+                {{-- Card Right --}}
+                <div class="card-right">
+                    <h5>ADVERTISEMENT</h5>
+                    <img src="{{ Vite::asset('resources/img/adv.jpg') }}" alt="ADV">
+                </div>
+            </div>
+        </div>
+    </section>
+@endsection
